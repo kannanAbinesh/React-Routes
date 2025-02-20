0f2904026af3235ba2dd453cc95fb0a9b7a7c66d
Abinesh <abineshkannan98@gmail.com>
2025-02-21 01:17:14 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+// /* Plugins. */
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+// /* Components. */
+// import Login from './Components/Login';
+// import Register from './Components/Register';
+// import Dashboard from './Components/Dashboard';
+// import PrivateRoutes from './Components/PrivateRoutes';
+
+// /* Styles. */
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <BrowserRouter>
+//         <Routes>
+//           <Route index path='/login' Component={Login} />
+//           <Route path='/register' Component={Register} />
+//           <Route Component={PrivateRoutes}>
+//             <Route path='/' Component={Dashboard} />
+//           </Route>
+//         </Routes>
+//       </BrowserRouter>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 /* Plugins. */
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 /* Components. */
 import Login from './Components/Login';
@@ -12,17 +43,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route index path='/login' Component={Login} />
-          <Route path='/register' Component={Register} />
-          <Route Component={PrivateRoutes}>
-            <Route path='/' Component={Dashboard} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route element={<PrivateRoutes />}>
+          <Route path="/" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </HashRouter>
   );
 }
 
