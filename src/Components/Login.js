3dcef236c37c4e9bc55e4f7da9c12763691bbf07
Abinesh <abineshkannan98@gmail.com>
2025-02-22 02:05:38 +0530

    login functionality

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,66 +1,104 @@
+/* Plugins. */
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
+/* Images. */
+import banner from '../Assets/Images/loginBanner.svg';
 
 const Login = () => {
-
     const navigate = useNavigate();
+    const [formData, setFormData] = useState({ email: "", password: "" });
 
     useEffect(() => {
         let localStorageData = localStorage.getItem('isLoggedIn');
-        if(localStorageData === "true") navigate('/') 
+        if (localStorageData === "true") {
+            navigate('/');
+        }
     }, [navigate]);
 
-    const [formData, setFormData] = useState({ email: "", password: "" });
-    const [error, setError] = useState("");
-
     const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
     };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        let users = JSON.parse(localStorage.getItem("users")) || [];
 
-        const user = users.find(
-            (user) => user.email === formData.email && user.password === formData.password
-        );
+        if (!formData.email || !formData.password) {
+            toast.error('Please fill in all fields');
+            return '';
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(formData.email)) {
+            toast.error('Invalid email format');
+            return '';
+        } else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/i.test(formData?.password)) {
+            toast.error('Invalid password.');
+            return '';
+        };
+
+        let users = JSON.parse(localStorage.getItem("users")) || [];
+        let user = users.find(user => user.email === formData.email && user.password === formData.password);
 
         if (user) {
             localStorage.setItem("isLoggedIn", true);
-            alert("Login Successful!");
             navigate('/')
-        } else {
-            setError("Invalid email or password");
-        }
+        } else toast.error('Invalid credentials.');
     };
 
     return (
-        <div className="register-form-wrapper">
-            <h2>Login</h2>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    value={formData.email}
-                    onChange={handleChange}
-                />
+        <div className="login-wrapper">
+            <div style={{ backgroundImage: `url(${banner})` }} className="login-banner" />
+            <div className="login-form-container">
+                <div className="login-form-wrapper">
 
-                <input
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    value={formData.password}
-                    onChange={handleChange}
-                />
+                    <div className="login-header-wrapper">
+                        <h2 className="login-header">Sign up</h2>
+                        <span className="login-second-header">
+                            Please enter Email and password.
+                        </span>
+                    </div>
 
-                {error && <p style={{ color: 'red' }}>{error}</p>}
+                    <form onSubmit={handleSubmit}>
+                        <div className="login-input-wrapper">
+                            <label className="login-input-label">Email</label>
+                            <input
+                                type="text"
+                                name="email"
+                                placeholder="Email"
+                                className="login-input-field"
+                                value={formData.email}
+                                onChange={handleChange}
+                            />
+                        </div>
 
-                <button type="submit" style={{ width: '90px', height: '30px', borderRadius: '5px', backgroundColor: 'red', cursor: 'pointer' }}>Login</button>
-                <div style={{ marginTop: '10px' }}>
-                    <Link to={'/register'} style={{ display: 'inline-block' }}>New to web page?</Link>
+                        <div className="login-input-wrapper">
+                            <label className="login-input-label">Password</label>
+                            <input
+                                type="password"
+                                name="password"
+                                placeholder="Password"
+                                className="login-input-field"
+                                value={formData.password}
+                                onChange={handleChange}
+                            />
+                        </div>
+
+                        <div className="login-btn-wrapper">
+                            <button type="submit" className="login-btn">
+                                Login
+                            </button>
+                        </div>
+
+                        <div className="login-link-btn-wrapper">
+                            <Link to={'/register'} className="login-link-btn">
+                                New to web page?
+                            </Link>
+                        </div>
+                    </form>
                 </div>
-            </form>
+            </div>
         </div>
     );
 };
